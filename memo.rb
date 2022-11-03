ea5bf6d70fed88c8b527ed7b9e60a362c1f24918
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2022-11-03 14:20:53 +0900

    ruby課題1

--- memo.rb
+++ memo.rb
@@ -6,22 +6,39 @@ memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換�
 
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
-
-
 if memo_type == 1
   puts "メモを入力してください"
   memo_text = gets.chomp
-  puts "拡張子を除いてファイル名を入力してください"
+  puts "拡張子を除いたファイル名を入力してください"
   memo_file = "#{gets.chomp}.csv"
-  CSV.open(memo_file, "w") do |csv|
-    csv << [memo_text]
+  begin
+    memo_input = CSV.read(memo_file)
+  rescue
+    CSV.open(memo_file, "w") do |csv|
+      csv << [memo_text]
+    end
+    puts "書き込みました"
+  else
+    puts "ファイルが既に存在しているので、書き込めません"
   end
+
 elsif memo_type == 2
-  puts "拡張子を除いてファイル名を入力してください"
+  puts "拡張子を除いたファイル名を入力してください"
   memo_file = "#{gets.chomp}.csv"
-  puts "メモを入力してください"
-  memo_text = gets.chomp
-  CSV.open(memo_file, "a") do |csv|
-    csv << [memo_text]
+  begin
+    memo_input = CSV.read(memo_file)
+  rescue
+    puts "ファイルがありません"
+  else
+    puts "メモの中身は以下のとおりです"
+    puts memo_input
+    puts "追記するメモを入力してください"
+    memo_text = gets.chomp
+    CSV.open(memo_file, "a") do |csv|
+      csv << [memo_text]
+    end
+    puts "書き込みました"
   end
+else
+  puts "1か2を入力してください"
 end
